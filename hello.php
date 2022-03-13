b37d844874a47d95e170a7bba1102254ba8881c6
OvadillaAisyahRahma <ovadillaaisyah@gmail.com>
2022-03-13 17:54:36 +0700

    membuat file hello.php

---
+++ hello.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>NO 1</title>
+</head>
+<body style="background-color:pink"> 
+    <?php
+    // Menuliskan nama dan nim 
+    echo "<h1> Nama : Ovadilla Aisyah Rahma <br/><h1>";
+    echo "NIM : 200411100033";
+    ?>
+</body> 
+</html>
