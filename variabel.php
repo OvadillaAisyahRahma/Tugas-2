d570887369466ed59238528f3150a5a2fa2b1ce4
OvadillaAisyahRahma <ovadillaaisyah@gmail.com>
2022-03-13 18:01:42 +0700

    membuat file variabel.php

---
+++ variabel.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>NO 2</title>
+</head>
+<body style="background-color:pink">
+    <?php
+    // 3 variabel yang berisi angka 
+    $k = 150;
+    $l = 20;
+    $m = 30;
+    // operasi penjumlahan, pengurangan, dan perkalian
+    $x1 = $k + $l + $m; 
+    $x2 = $k - $l - $m;
+    $x3 = $l * $m;
+    echo "Hasil Penjumlahan = $x1 <br/>";
+    echo "Hasil Pengurangan = $x2 <br/>";
+    echo "Hasil Perkalian = $x3 <br/>";
+    ?>
+</body>
